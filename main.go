49892891a08f758c187ceccbf1854324dde44444
austinclemmer <clemmerag@appstate.edu>
2018-02-07 16:36:56 -0500

    half of skateboard

--- main.go
+++ main.go
@@ -3,20 +3,66 @@ package main
 import (
 	"bufio"
 	"fmt"
-	//	"github.com/pkg/term"
+	"github.com/pkg/term"
 	"os"
+	"os/exec"
 )
 
+type Command string
+
 func main() {
+	//fmt.Printf("bush >")
 
-	fmt.Printf("bush >")
+	//initialize terminal
+	terminal, err := term.Open("/dev/tty")
+	if err != nil {
+		panic(err)
+	}
 
-	reader := bufio.NewReader(os.Stdin)
+	//set restore point and restore term at end of program
+	defer terminal.Restore()
+	terminal.SetCbreak()
+
+	//initialize buffer and reader
+	reader := bufio.NewReader(terminal)
+
+	//string command variable (global)
+	var cmd Command
+
+	//read in the characters/ build the command string
+	//panic if errors
 	for {
 		character, _, err := reader.ReadRune()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(character)
+		switch character {
+		case '\n':
+			fmt.Printf("\n")
+			//handle command
+			if cmd == "exit" || cmd == "quit" || cmd == "bye" {
+				os.Exit(0)
+			} else {
+				err := cmd.HandleCmd()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%v\n", err)
+				}
+			}
+			cmd = ""
+
+		case '\u007f':
+			if len(cmd) > 0 {
+				cmd = cmd[:len(cmd)-1]
+				fmt.Printf("\u0008 \u0008")
+			}
+		default:
+			fmt.Printf("%c", character)
+			cmd += Command(character)
+		}
 	}
+}
+
+func (comm Command) HandleCmd() error {
+	cmd := exec.Command(string(comm))
+	return cmd.Run()
 }
