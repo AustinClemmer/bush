15333dd3e2f0fdb6405699a59a37a3938ea90ddd
austinclemmer <clemmerag@appstate.edu>
2018-04-29 10:41:06 -0400

    cd home test implemented

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -13,8 +14,13 @@ func TestPrintedOutputList(t *testing.T) {
 }
 func TestHome(t *testing.T) {
 	status := executor("cd")
+	hd := os.Getenv("HOME")
+	wd, _ := os.Getwd()
 
 	if status != nil {
 		t.Error("Failed to exit cleanly")
+	}
+	if hd != wd {
+		t.Error("Directories mismatched, failed to return home")
 	}
 }
